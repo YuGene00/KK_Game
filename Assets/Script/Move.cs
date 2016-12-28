84ad830d810a2ecfe6aece7a40ffba1930ace5a7
bbiggoagi <bbiggoagi@gmail.com>
2016-12-28 19:31:24 +0900

    프로젝트 초기 커밋

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move {
+
+    //variable
+    AreaController areaController;
+
+    public Move() {
+        areaController = new AreaController();
+    }
+
+    public void SetMovableArea(Vector2 leftBottom, Vector2 rightTop) {
+        areaController.SetArea(leftBottom, rightTop);
+    }
+
+	public Transform MoveTransToDestInArea(Transform moveTarget, Vector2 dest) {
+        moveTarget.position = areaController.FixPositionInArea(dest);
+        return moveTarget;
+    }
+
+    public Transform MoveTransToDest(Transform moveTarget, Vector2 dest) {
+        moveTarget.position = dest;
+        return moveTarget;
+    }
+
+    public bool IsInArea(Vector2 origin) {
+        return areaController.IsInArea(origin);
+    }
+}
